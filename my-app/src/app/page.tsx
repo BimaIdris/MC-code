a3a7508c15955eedf5007658d4eda98badb8f23a
BimaIdris <bimaidris@gmail.com>
2025-11-15 20:11:26 +0100

    Second Comit

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -3,8 +3,17 @@ import {
   RegisterLink,
   LoginLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
-const home = () => {
+const home = async () => {
+  const { isAuthenticated, getUser } = getKindeServerSession(); //useAuth();
+  const user = getUser();
+  console.log("User Info:", user);
+  // if (!(await isAuthenticated())) {
+  //   redirect("api/auth/login");
+  // }
   return (
     <div>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100 px-4">
@@ -15,23 +24,35 @@ const home = () => {
           <p className="text-lg text-gray-700 mb-6">
             This is a protected homepage. Authentication is powered by Kinde.
           </p>
-          {/* <div className="flex justify-center gap-4">
-            <LoginLink>Sign in</LoginLink>
-          </div> */}
           <div className="flex justify-center gap-4 mt-4">
-            <LoginLink>
-              <button className="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
-                Login
-              </button>
-            </LoginLink>
-            <Link href="#">
-              <button className="px-6 py-2 bg-green-600 text-white rounded hover:bg-green-700">
-                Register
-              </button>
-            </Link>
+            {isAuthenticated() ? (
+              <>
+                <Link href="/dashboard">
+                  <Button>Go to Dashboard</Button>
+                </Link>
+              </>
+            ) : (
+              <>
+                <LoginLink>
+                  <button className="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+                    Login
+                  </button>
+                </LoginLink>
+                <Link href="#">
+                  <button className="px-6 py-2 bg-green-600 text-white rounded hover:bg-green-700">
+                    Register
+                  </button>
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </main>
+      <footer className="fixed bottom-0 w-full bg-gray-200 text-center py-2">
+        <p className="text-sm text-gray-600">
+          &copy; {new Date().getFullYear()} MC. All rights reserved.
+        </p>
+      </footer>
     </div>
   );
 };
